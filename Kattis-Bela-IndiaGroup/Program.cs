9d24f3c5787c0a783310d37788718bc50074b603
seanherbert105 <sherbert105@gmail.com>
2022-02-20 10:57:11 -0500

    Secondary changes applied

--- Kattis-Bela-IndiaGroup/Program.cs
+++ Kattis-Bela-IndiaGroup/Program.cs
@@ -10,14 +10,38 @@
 Random random = new Random();
 
 
-// list values of dominant and non dominant
-int[] dominant = { 11, 4, 3, 20, 10, 14, 0, 0 };
-int[] nonDominant = { 11, 4, 3, 2, 10, 0, 0, 0 };
-
-// list different types of suit available
+// list different types of card suits available
 string[] suit = { "S", "H", "D", "C" };
 
-// choose a random suit from the suit list
-int dominantSuit = random.Next(suit.Length);
+// choose a random card suit
+int randomSuit = random.Next(suit.Length);
+string dominantSuit = suit[randomSuit];
+Console.WriteLine("The Dominant Suit is:  " + dominantSuit);
 
-Console.WriteLine("The Dominant Suit is:  " + suit[dominantSuit]);
+
+// loop through the card suits
+for (int i = 0; i < 100; i++)
+{
+    if (dominantSuit == dominantSuit)
+    {
+        // the dominant values
+        int[] dominant = { 11, 4, 3, 20, 10, 14, 0, 0 };
+
+        // choose a random non dominant value
+        int randomDominant = random.Next(dominant.Length);
+        Console.WriteLine(randomDominant + suit[randomSuit]);
+        i++;
+    }
+    else
+    {
+        //the non dominant values
+        int[] nonDominant = { 11, 4, 3, 2, 10, 0, 0, 0 };
+
+        // choose a random non dominant value
+        int randomNonDominant = random.Next(nonDominant.Length);
+        Console.WriteLine(randomNonDominant + suit[randomSuit]);
+        i++;
+    }
+}
+
+
