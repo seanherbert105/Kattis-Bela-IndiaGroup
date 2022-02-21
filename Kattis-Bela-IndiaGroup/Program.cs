576ea694dda0bee1f41717c33f8a5c0aa76e6ef1
seanherbert105 <sherbert105@gmail.com>
2022-02-21 10:14:59 -0500

    New changes have been made, code functions as intended.

--- Kattis-Bela-IndiaGroup/Program.cs
+++ Kattis-Bela-IndiaGroup/Program.cs
@@ -9,39 +9,87 @@
 // random number object
 Random random = new Random();
 
-
-// list different types of card suits available
-string[] suit = { "S", "H", "D", "C" };
-
-// choose a random card suit
-int randomSuit = random.Next(suit.Length);
-string dominantSuit = suit[randomSuit];
-Console.WriteLine("The Dominant Suit is:  " + dominantSuit);
+// error message for invalid number range for the input
+string numberOfHandsError = "User must choose between 0 to 100";
 
 
-// loop through the card suits
-for (int i = 0; i < 100; i++)
+// number of hands input
+string numberOfHandsInput = Console.ReadLine();
+int numberOfHands = int.Parse(numberOfHandsInput);
+
+if (numberOfHands == 0)
 {
-    if (dominantSuit == dominantSuit)
-    {
-        // the dominant values
-        int[] dominant = { 11, 4, 3, 20, 10, 14, 0, 0 };
-
-        // choose a random non dominant value
-        int randomDominant = random.Next(dominant.Length);
-        Console.WriteLine(randomDominant + suit[randomSuit]);
-        i++;
-    }
-    else
-    {
-        //the non dominant values
-        int[] nonDominant = { 11, 4, 3, 2, 10, 0, 0, 0 };
-
-        // choose a random non dominant value
-        int randomNonDominant = random.Next(nonDominant.Length);
-        Console.WriteLine(randomNonDominant + suit[randomSuit]);
-        i++;
-    }
+    Console.WriteLine(numberOfHandsError);
+}
+else if (numberOfHands > 100)
+{
+    Console.WriteLine(numberOfHandsError);
+}
+else
+{
+    Console.WriteLine(numberOfHands);
 }
 
 
+// error message for invalid suit for the input
+string suitError = "User must choose between the following suits - S, H, D, C";
+
+
+// list different types of card suits available
+string[] suit = { "S", "H", "D", "C" };
+string userSuitInput = Console.ReadLine();
+string dominantSuit = userSuitInput;
+
+if (userSuitInput == "S" || userSuitInput == "s")
+{
+    Console.WriteLine(userSuitInput);
+}
+else if (userSuitInput == "H" || userSuitInput == "h")
+{
+    Console.WriteLine(userSuitInput);
+}
+else if (userSuitInput == "D" || userSuitInput == "d")
+{
+    Console.WriteLine(userSuitInput);
+}
+else if (userSuitInput == "C" || userSuitInput == "c")
+{
+    Console.WriteLine(userSuitInput);
+}
+else
+{
+    Console.WriteLine(suitError);
+}
+
+
+int total = 0;
+
+// loop through the card suits
+for (int i = 0; i < numberOfHands * 2; i++)
+{
+    // the dominant values
+    int[] dominant = { 11, 4, 3, 20, 10, 14, 0, 0 };
+    // choose a random non dominant value
+    int dominantRandomValue = random.Next(0, dominant.Length);
+
+    //the non dominant values
+    int[] nonDominant = { 11, 4, 3, 2, 10, 0, 0, 0 };
+    // choose a random non dominant value
+    int nonDominantRandomValue = random.Next(0, nonDominant.Length);
+
+    int randomSuit = random.Next(0, suit.Length);
+    string newRandomSuit = randomSuit.ToString();
+
+    if (dominantSuit == newRandomSuit)
+    {
+        Console.WriteLine(dominantRandomValue + suit[randomSuit]);
+        total += dominantRandomValue;
+    }
+    else
+    {
+        Console.WriteLine(nonDominantRandomValue + suit[randomSuit]);
+        total += nonDominantRandomValue;
+    }
+}
+
+Console.WriteLine(total);
